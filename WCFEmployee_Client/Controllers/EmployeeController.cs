ca081a5bd21015b2f739691cf9554d7295d3ecea
quytitans <78024669+quytitans@users.noreply.github.com>
2022-05-10 21:49:55 +0700

    submit

---
+++ WCFEmployee_Client/Controllers/EmployeeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WCFEmployee_Client.ServiceReference1;
+
+namespace WCFEmployee_Client.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private Service1Client serviceAClient;
+
+        public EmployeeController()
+        {
+            serviceAClient = new Service1Client();
+        }
+        // GET: Employee
+        public ActionResult Index(string searchString)
+        {
+            ViewBag.CurrentFilter = searchString;
+
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                var employeeList = serviceAClient.SearchByDepartment(searchString);
+                return View(employeeList);
+            }
+
+            return View(serviceAClient.GetAll());
+        }
+        // GET: Employee/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employee/Create
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            var result = serviceAClient.AddEmployee(employee);
+            if (result != null)
+            {
+                var list = serviceAClient.GetAll();
+                return View("Index", list);
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+    }
+}
---
+++ WCFEmployee_Client/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WCFEmployee_Client")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WCFEmployee_Client")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f53d51d6-9933-4d60-a506-8ac23b0bd721")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
